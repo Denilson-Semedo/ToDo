0190e13786358d12000044ec1ea3700a1b857e5d
Denilson Semedo Tavares <dstavares00@gmail.com>
2022-04-19 10:21:00 +0100

    Cretate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import { ImportantComponent } from './important/important.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TodayComponent } from './today/today.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImportantComponent,
+    TasksComponent,
+    TodayComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
