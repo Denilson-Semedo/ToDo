7a8efb75030d9e2458747475fcc44fe4ff803f82
Denilson Semedo Tavares <dstavares00@gmail.com>
2022-04-26 09:42:10 +0100

    switch childs components in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ToDoList';
   showFiller = false;
+  menu = "menu";
+  tasks = false
+  today = true;
+  important = false;
 }
