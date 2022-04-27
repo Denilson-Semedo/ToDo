312462ab9c568fb3a2b6b3142f4182d304cfc8d1
Denilson Semedo Tavares <dstavares00@gmail.com>
2022-04-27 01:05:34 +0100

    configure the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { TasksComponent } from './tasks/tasks.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TodayComponent } from './today/today.component';
+import { ImportantComponent } from './important/important.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'today', pathMatch: 'full' },
+  { path: 'today', component: TodayComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'important', component: ImportantComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
